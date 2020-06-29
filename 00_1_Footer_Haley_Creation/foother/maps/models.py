1c96676e6a2314b2d0e7dc7fd00cf9aab82db4b1
JihyeHaley <ohhojh@gmail.com>
2020-06-29 13:00:31 +0900

    commit for all pull

--- 00_1_Footer_Haley_Creation/foother/maps/models.py
+++ 00_1_Footer_Haley_Creation/foother/maps/models.py
@@ -4,14 +4,10 @@ from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 
 # Create your models here.
-class Map(models.Model):
-    lat = models.FloatField()
-    lng = models.FloatField()
-    #뭔가 더 정해야 한다.
-
-
 class Upload(models.Model):
     restaurant_address = models.CharField(max_length=100)
+    lat = models.FloatField()
+    lng = models.FloatField()
     restaurant_name = models.CharField(max_length=100)
     food_name = models.CharField(max_length=100)
     food_star = models.FloatField()
@@ -23,4 +19,4 @@ class Upload(models.Model):
                     'quality': 80}
     )
     upload_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    
+    user_upload = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='user_upload')
